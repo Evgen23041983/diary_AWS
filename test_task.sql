f9b4e6ef61208e8f37ef327a6f58e90360b00b4b
Evgen <evgen23041983@gmail.com>
2020-03-06 00:25:53 +0200

    test2

--- test_task.sql
+++ test_task.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 02 2020 г., 01:20
+-- Время создания: Мар 06 2020 г., 01:25
 -- Версия сервера: 8.0.15
 -- Версия PHP: 7.0.33
 
@@ -30,36 +30,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tasks` (
   `id` int(11) NOT NULL,
+  `member` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `task` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `done` int(11) NOT NULL DEFAULT '1'
+  `done` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `task`, `done`) VALUES
-(1, '5555', 1),
-(2, '5555', 1),
-(3, 'task', 1),
-(4, 'task', 1),
-(5, 'task', 1),
-(6, 'task', 1),
-(7, 'task', 1),
-(8, 'task', 1),
-(9, 'task', 1),
-(10, '222', 1),
-(11, 'task', 1),
-(12, '3333', 1),
-(13, '5555', 1),
-(14, 't55', 1),
-(15, 'task', 1),
-(16, 'task', 1),
-(17, 'task', 1),
-(18, 'task', 1),
-(19, '1545', 1),
-(20, '1545', 1),
-(21, '1545', 1);
+INSERT INTO `tasks` (`id`, `member`, `task`, `done`) VALUES
+(1, 'son', 'vacuum', 'Yes'),
+(2, 'mather', 'wash', 'No'),
+(3, 'daughter', 'to wash the dishes', 'No'),
+(4, 'mather', 'make dinner', 'Yes');
 
 -- --------------------------------------------------------
 
@@ -79,11 +63,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `login`, `password`) VALUES
-(1, '', 'mather', '$2y$11$8ee82d517805e978f4687eV2H1xjYlA.vbfB8Qnx8UcGH/3Hbh4gm'),
-(2, '', 'father', '$2y$11$e5a4a889c4024de251f23Ohfc83/YsiQzPGApBaV4S9xasO2ZIexq'),
-(3, 'evgen_kalina@mail.ru', 'admin', '$2y$11$66b722e6ccba8c1858c01OgNuBUnxbL08yEHFC1t8clrqdWKFv2WG'),
-(4, 'evgen23041983@gmail.com', '111', '$2y$11$74d93b9e630bf4393780cO9QIYbbdW9gCJEnEb0obsGI.u7fT8NuC'),
-(5, 'evgen@gmail.com', 'fhather', '$2y$11$f858ca35e8be819d895dfujMgWv7vj3QL692lUydIsOSXZEEfw26a');
+(1, 'mather@gmail.com', 'mather', '$2y$11$e3aa24f18669fb7b50d43uz6rpW0BXkwezD65nmJLWm6YirM9GPZi'),
+(2, 'father@gmail.com', 'father', '$2y$11$b366ac3c11fd3226dc3d1OqmS0ML6sQ3QlfPlQI7NwvWLlmWcYyiW'),
+(3, 'child@gmail.com', 'child', '$2y$11$0c0de3d36c14785796100uk6yHxVwv4hNDfpvwBBSrzCPMQ/yCvDu'),
+(4, 'child1@gmail.com', '333', '$2y$11$b0bbaae829d4181ec9a1fupvIW8ZvK2jcQJ23w3mBJkFT1ExAGa7W');
 
 --
 -- Индексы сохранённых таблиц
@@ -93,13 +76,15 @@ INSERT INTO `users` (`id`, `email`, `login`, `password`) VALUES
 -- Индексы таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `member` (`member`);
 
 --
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `login` (`login`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -109,13 +94,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
